bb3e56858c7900580adf69a03245eaa024d23c48
Darren <128713023+d-kingbarou@users.noreply.github.com>
2023-08-17 19:37:20 +0800

    Add files via upload

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Stack, Box } from '@mui/material';
+import VideoCard from './VideoCard';
+import ChannelCard from './ChannelCard';
 
 const Videos = ({ videos }) => {
   return (
-    <div>Videos</div>
+    <Stack 
+     direction="row" 
+     flexWrap="wrap" 
+     justifyContent="start" 
+     gap={2}
+    >
+      {videos.map((item, index) =>(
+        <Box key={index}>
+          {/* Takes into consideration what the API returns and what to present on the app */}
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
--- src/containers/ChannelDetail.jsx
+++ src/containers/ChannelDetail.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Videos, ChannelCard } from '../components';
+import { fetchFromAPI } from '../constants/fetchFromAPI';
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  
+  // using useParams to get access to id inside this code
+  const { id } = useParams();
+
+  // re-renders webpage everytime id changes
+  useEffect(() => {
+    // fetches channel data for channel page
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+
+    // fetches videos data on the channel page
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items));
+  }, [id])
+
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+      <Box>
+        {/* top banner gradient cover image */}
+        <div style={{
+          height:'280px',
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+        }} />
+
+        {/* reused channelcard component to render channel icon, title & sub count  */}
+        {/* Note: margintop is used dynamically in channelcard code: margintop is 
+        passed as a prop and only adjusts margintop when passed in code in other file i.e.: here */}
+        <ChannelCard channelDetail={channelDetail} marginTop="-120px"  />
+      </Box>
+
+      <Box display="flex" p="2">
+        {/* render the videos specific to the channel */}
+        <Box sx={{ mr: { sm:'130px' }}} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
   )
 }
 
